0ce16ccbdfb8577300ad59e5a56da802d1251bd5
Colin Miller <colinamiller210@gmail.com>
2022-02-25 21:35:39 -0500

    Added some logic for driver and rider classes. Debating making add_rider in driver class iterate through list of riders rather than dealing with a rider individually.

--- carpooling_main.py
+++ carpooling_main.py
@@ -5,9 +5,10 @@ sheet = service_account.open("Fall 2021 Second Concert Orchestras Carpool Survey
 response_worksheet = sheet.worksheet("Form Responses 1")
 
 all_orchestras_info = response_worksheet.get_all_records()
-all_philharmonic_info = sheet.add_worksheet(title="All Philharmonic", rows="100", cols="20")
-all_symphony_info = sheet.add_worksheet(title="All Symphony", rows="100", cols="20")
-all_string_info = sheet.add_worksheet(title="All String", rows="100", cols="20")
+
+philharmonic_assignments = sheet.add_worksheet(title="Philharmonic Assignments", rows="100", cols="20")
+symphony_assignments = sheet.add_worksheet(title="Symphony Assignments", rows="100", cols="20")
+string_assignments = sheet.add_worksheet(title="String Assignments", rows="100", cols="20")
 
 philharmonic_riders = []
 symphony_riders = []
@@ -41,3 +42,26 @@ for response in all_orchestras_info:
             string_drivers.append(response)
         else:
             pass
+        
+class driver:
+    def __init__(self, google_sheet_response):
+        self.contact_info = [google_sheet_response[1], google_sheet_response[3], google_sheet_response[4], google_sheet_response[5]]
+        self.remaining_seats = google_sheet_response[-2]
+        self.riders = []
+    
+    def add_rider(self, rider_instance):
+        if self.remaining_seats < rider_instance.seat_value:
+            return
+        else:
+            self.riders.append(rider_instance)
+            self.remaining_seats -= rider_instance.seat_value
+            
+         
+class rider:
+    def __init__(self, google_sheet_response):
+        self.contact_info = self.contact_info = [google_sheet_response[1], google_sheet_response[3], google_sheet_response[4], google_sheet_response[5]]
+        if self.contact_info[1] == 'Cello':
+            self.seat_value = 2
+        else:
+            self.seat_value = 1
+            
